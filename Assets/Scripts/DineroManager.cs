0da06df641af113a111418c8a25449be6ace44a0
micabod <jeronimosanchezperuga@gmail.com>
2025-05-30 09:28:50 -0300

    Mercancia y DIneroManager

---
+++ Assets/Scripts/DineroManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DineroManager : MonoBehaviour
+{
+    public float playerMoney;
+
+    public void UpdateMoney(float amount)
+    {
+
+        if (playerMoney + amount < 0)
+        {
+            //impedir compra
+        }
+        else
+        {
+            playerMoney += amount;
+        }
+    }
+}
---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio = 10;
+    public DineroManager dineroManager;
+
+    void Start()
+    {
+        dineroManager = FindObjectOfType<DineroManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //restar precio al dinero del player
+            dineroManager.UpdateMoney(-precio);
+            Destroy(gameObject);
+        } 
+    }
+}
